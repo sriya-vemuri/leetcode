6293302eb017e9f2929365f63e11ce472100a689
Sriya Vemuri <84768384+sriya-vemuri@users.noreply.github.com>
2022-05-20 16:03:26 +0530

    Create GFG_Sort012.c

---
+++ GFG_Sort012.c
@@ -0,0 +1,22 @@
+void swap(int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void sort012(int a[], int n)
+{
+    int l=0,m=0,h=n-1;
+    while(m<=h){
+        if(a[m]==0){
+            swap(&a[l++],&a[m++]);
+        }
+            
+            else if(a[m]==1)
+            m++;
+            
+            else
+            swap(&a[m],&a[h--]);
+            
+        }
+    }

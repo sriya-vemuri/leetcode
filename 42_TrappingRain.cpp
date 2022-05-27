738850c8f6ceae1ff5fcdb099beb21a250ef357b
Sriya Vemuri <84768384+sriya-vemuri@users.noreply.github.com>
2022-05-27 22:49:18 +0530

    Create 42_TrappingRain.cpp

---
+++ 42_TrappingRain.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+  int prefix[n], suffix[n];
+  prefix[0] = height[0];
+  for (int i = 1; i < n; i++) {
+    prefix[i] = max(prefix[i - 1], height[i]);
+  }
+for(int i=0;i<n;i++){cout<<prefix[i]<<" ";}
+  suffix[n - 1] = height[n - 1];
+  for (int i = n - 2; i >= 0; i--) {
+    suffix[i] = max(suffix[i + 1], height[i]);
+  }
+        cout<<"/n";
+        for(int i=0;i<n;i++){cout<<suffix[i]<<" ";}
+cout<<endl;
+  int waterTrapped = 0;
+  for (int i = 0; i < n; i++) {
+    int t= min(prefix[i], suffix[i]) - height[i];
+      cout<<t<<" ";
+      waterTrapped+=t;
+  }
+  return waterTrapped;
+
+    }
+};

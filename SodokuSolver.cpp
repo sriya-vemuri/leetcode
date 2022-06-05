c895f4118068fbe6ebb43c2c5508c657ae473f63
Sriya Vemuri <84768384+sriya-vemuri@users.noreply.github.com>
2022-06-05 12:15:57 +0530

    Create SodokuSolver.cpp

---
+++ SodokuSolver.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    bool valid(char c,vector<vector<char>>& board,int row,int col){
+        for(int i=0;i<9;i++){
+            if(board[i][col]==c) return false;
+            if(board[row][i]==c) return false;
+            if(board[3*(row/3)+(i/3)][3*(col/3)+(i%3)]==c) return false;
+        }
+        return true;
+    }
+    
+    bool solve(vector<vector<char>>& board) {
+        int n=size(board);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]=='.'){
+                for(char c='1';c<='9';c++){
+                    if(valid(c,board,i,j)){
+                        board[i][j]=c;
+                        if(solve(board)==true) return true;
+                       else board[i][j]='.';
+                    }
+                }
+                     return false;
+            }
+               
+            }
+        }
+        return true;
+    }
+    void solveSudoku(vector<vector<char>> & board){
+        solve(board);
+    }
+};
